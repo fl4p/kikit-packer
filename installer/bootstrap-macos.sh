b2a89e7086bef615d01ada3d42279bfe5b12c805
Fabian <fabian@schlieper.email>
2026-08-24 11:27:11 +0200

    Pin bootstrap to durable installer recovery
    Bind the immutable bootstrap archive to application commit 43286bd and its verified codeload SHA-256.

--- installer/bootstrap-macos.sh
+++ installer/bootstrap-macos.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -eu
 
-SOURCE_REF=666e17c3c7c1b278b42b1041dd58fbe01fb97373
-SOURCE_SHA256=78b87b76cab5eb2f100945b3236fe44c65243ef487a23244109b98ea958cc9ed
+SOURCE_REF=43286bd2f97a3f36d97fef67e27c20e4ef45d0b0
+SOURCE_SHA256=34003c971680124c3b1eeb77ff35d1178495835bc584feff36c1071493d365d3
 SOURCE_URL="https://codeload.github.com/fl4p/kikit-packer/tar.gz/$SOURCE_REF"
 
 workdir=$(mktemp -d "${TMPDIR:-/tmp}/kikit-packer-bootstrap.XXXXXX")
